1f482f885d988bee7a770128c85c38a23ba50587
Eldego07 <diego.casolaro8@gmail.com>
2025-12-03 09:21:28 +0100

    Initial commit

---
+++ src/simulatorecalcio/Partita.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simulatorecalcio;
+
+/**
+ *
+ * @author casolaro.diego
+ */
+public class Partita {
+    private Squadra squadraCasa;
+    private Squadra squadraTrasferta;
+    private String risultato;
+    
+    public Partita(Squadra squadraCasa, Squadra squadraTrasferta){
+        this.squadraTrasferta=squadraTrasferta;
+    }
+}
