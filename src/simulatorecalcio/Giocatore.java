cce637f4dfb33e80077c02abca4b71d883aaa467
TaboadaTaddeo22 <taboada.taddeo@franchettisalviani.net>
2025-12-03 09:43:57 +0100

    Update Giocatore.java

--- src/simulatorecalcio/Giocatore.java
+++ src/simulatorecalcio/Giocatore.java
@@ -4,10 +4,69 @@
  */
 package simulatorecalcio;
 
+import java.util.Objects;
+
 /**
  *
  * @author casolaro.diego
  */
 public class Giocatore {
     protected String nome;
+    protected String cognome;
+    protected String forza;
+    
+    public Giocatore(String nome, String cognome, String forza) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.forza = forza;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getForza() {
+        return forza;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setForza(String forza) {
+        this.forza = forza;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Giocatore other = (Giocatore) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return Objects.equals(this.cognome, other.cognome);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " " + cognome + ", " + forza + " di forza";
+    }
+    
+    
 }
